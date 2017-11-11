04eefe24123d8d67501ef2a122d9502d2c71e2fa
Sammy Mwangangi <sammymwinzi2@gmail.com>
2017-11-11 10:41:04 +0300

    updated settings.py
    Removed 'BLOG_SLUG '

--- redapple/settings.py
+++ redapple/settings.py
@@ -85,8 +85,6 @@ from django.utils.translation import ugettext_lazy as _
 # INSTALLED_APPS setting.
 USE_MODELTRANSLATION = False
 
-BLOG_SLUG = ""
-
 ########################
 # MAIN DJANGO SETTINGS #
 ########################
